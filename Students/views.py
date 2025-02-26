2bfd89ed09fd36b305f3931c8d85b1a115fd7ef0
vasilij korobov <kavyabass51@gmail.com>
2025-02-26 19:50:51 +0700

    добавили в список установленных приложений Студентс. А так же написали простые функции в контроллеры

--- Students/views.py
+++ Students/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def example_view(request):
+    return render(request, 'app/example.html')
+
+
+def show_data(request):
+    if request.method == 'GET':
+        return render(request, 'app/show_data.html')
+
+
+def submit_data(request):
+    # request.GET   - это словари хранящие в себе GET(получить)
+    # request.POST  - и POST(отправить) параметры запроса соответственно.
+    if request.method == 'POST':
+        return HttpResponse("Данные отправлены")
