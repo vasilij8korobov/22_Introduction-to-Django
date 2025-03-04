6e05ad80f2af61549496808cc98af70451216fc0
vasilij korobov <kavyabass51@gmail.com>
2025-03-05 00:41:21 +0700

    4:16

--- Students/models.py
+++ Students/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name="Имя")
+    last_name = models.CharField(max_length=150, verbose_name="Фамилия")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "студент"
+        verbose_name_plural = "студенты"
+        ordering = ["last_name"]
+        db_table = "custom_table_name"
