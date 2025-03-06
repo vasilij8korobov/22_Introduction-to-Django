aa1ca8f0ebd1dded4df0317f4cd465f5351da5e2
vasilij korobov <kavyabass51@gmail.com>
2025-03-06 21:17:15 +0700

    Работа с моделями. Всё указано в подсказке4(но требует форматирования)

--- Students/models.py
+++ Students/models.py
@@ -1,15 +1,46 @@
 from django.db import models
 
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=150, verbose_name="Имя")
-    last_name = models.CharField(max_length=150, verbose_name="Фамилия")
+class Group(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.name
 
-    class Meta:
-        verbose_name = "студент"
-        verbose_name_plural = "студенты"
-        ordering = ["last_name"]
-        db_table = "custom_table_name"
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='Students')
+
+    def __str__(self):
+        return self.name
+
+    # first_name = models.CharField(max_length=150, verbose_name="Имя")
+    # last_name = models.CharField(max_length=150, verbose_name="Фамилия", unique=True)
+    #
+    # age = models.IntegerField(help_text='Введите возраст студента')
+    # is_active = models.BooleanField(default=True)
+    # description = models.TextField(null=True, blank=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # image = models.ImageField(upload_to='photos/', verbose_name='Фотография')
+    """
+    Для работы с полем ImageField необходимо установить библиотеку Pillow
+    """
+    # group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='Students')
+    # profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    # tags = models.ManyToManyField(Tag)
+    #
+    # STATUS_CHOICES = [
+    #     ('draft', 'Draft'),
+    #     ('published', 'Published'),
+    # ]
+    # status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    #
+    # def __str__(self):
+    #     return f"{self.first_name} {self.last_name}"
+    #
+    # class Meta:
+    #     verbose_name = "студент"
+    #     verbose_name_plural = "студенты"
+    #     ordering = ["last_name"]
+    #     db_table = "custom_table_name"
