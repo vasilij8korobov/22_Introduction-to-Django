ec32d5f1943e33bff5175f6bed651fb5cad513a4
vasilij korobov <kavyabass51@gmail.com>
2025-03-06 23:21:48 +0700

    Миграции 12:06

--- Students/models.py
+++ Students/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 
 
-class Group(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='Students')
+    FIRST_YEAR = 'first'
+    SECOND_YEAR = 'second'
+    THIRD_YEAR = 'third'
+    FOURTH_YEAR = 'fourth'
+
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FIRST_YEAR, 'first'),
+        (SECOND_YEAR, 'second'),
+        (THIRD_YEAR, 'third'),
+        (FOURTH_YEAR, 'fourth'),
+    ]
+
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    year = models.CharField(max_length=6, choices=YEAR_IN_SCHOOL_CHOICES, default=FIRST_YEAR, verbose_name='Курс')
 
     def __str__(self):
-        return self.name
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+        ordering = ['last_name']
+
 
     # first_name = models.CharField(max_length=150, verbose_name="Имя")
     # last_name = models.CharField(max_length=150, verbose_name="Фамилия", unique=True)
