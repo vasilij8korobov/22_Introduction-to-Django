44efee7a0c4e7fd27cb5e05377549434c7e2792b
vasilij korobov <kavyabass51@gmail.com>
2025-03-15 16:11:50 +0700

    переопределение методов

--- Students/views.py
+++ Students/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 
 from Students.models import Student, MyModel
-
 
 """ниже идут CBV контроллеры"""
 
@@ -16,17 +15,66 @@ class MyModelCreateView(CreateView):
     template_name = 'Students/mymodel_form.html'
     success_url = reverse_lazy('Students:mymodel_list')
 
+    def form_valid(self, form):
+        """
+        form_valid() — используется для обработки данных,
+        если форма прошла валидацию.
+        Этот метод вызывается после успешной валидации формы и
+        сохранения данных
+        """
+        form.instance.created_by = self.request.user
+
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        """
+        form_invalid() — используется для обработки данных,
+        если форма не прошла валидацию.
+        Этот метод вызывается при ошибках валидации формы
+        """
+        response = super().form_invalid(form)
+        response.context_data['error_message'] = 'Please correct the errors'
+
+        return response
+
 
 class MyModelListView(ListView):
     model = MyModel
     template_name = 'Students/mymodel_list.html'
     context_object_name = 'mymodels'
 
+    def get_queryset(self):
+        """
+        get_queryset() — используется для получения набора данных,
+        который будет отображаться в представлении.
+        Этот метод позволяет настроить выборку данных из базы данных
+        """
+        queryset = super().get_queryset().filter(is_active=True)
+        return queryset
+
 
 class MyModelDetailView(DetailView):
     model = MyModel
     template_name = 'Students/mymodel_detail.html'
     context_object_name = 'mymodel'
+
+    def get_additional_data(self):
+        return 'Это дополнительная информация'
+
+    def get_context_data(self, **kwargs):
+        """
+        get_context_data() — используется для добавления данных в контекст шаблона.
+        Этот метод позволяет передавать дополнительные данные в шаблон для рендеринга
+        """
+        context = super().get_context_data(**kwargs)
+        context['additional_data'] = self.get_additional_data()
+        return context
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if not obj.is_active:
+            raise Http404('Object not found')
+        return obj
 
 
 class MyModelUpdateView(UpdateView):
