ed1811541a7ae59f49265b5102a63bd9423b6656
vasilij korobov <kavyabass51@gmail.com>
2025-03-21 13:12:43 +0700

    Стилизация через __init__, шаблон формы стал красивее

--- Students/forms.py
+++ Students/forms.py
@@ -8,6 +8,33 @@ class StudentForm(forms.ModelForm):
         model = Student
         fields = ['first_name', 'last_name', 'year', 'email', 'enrollment_date', ]
 
+    def __init__(self, *args, **kwargs):
+        super(StudentForm, self).__init__(*args, **kwargs)
+
+        # Настройка атрибутов виджета для поля 'first_name'
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
+            'placeholder': 'Введите имя'  # Текст подсказки внутри поля
+        })
+
+        # Настройка атрибутов виджета для поля 'last_name'
+        self.fields['last_name'].widget.attrs.update({
+            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
+            'placeholder': 'Введите фамилию'  # Текст подсказки внутри поля
+        })
+
+        # Настройка атрибутов виджета для поля 'email'
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
+            'placeholder': 'Введите email'  # Текст подсказки внутри поля
+        })
+
+        # Настройка атрибутов виджета для поля 'enrollment_date'
+        self.fields['enrollment_date'].widget.attrs.update({
+            'class': 'form-control',  # Добавление CSS-класса для стилизации поля
+            'type': 'date'  # Указание типа поля как даты
+        })
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         suffix = ('@example.com', '@gmail.com', '@mail.ru',)
