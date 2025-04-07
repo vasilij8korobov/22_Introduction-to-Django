05bd8956d1e1fb8f73bf144a7288719d13174f43
vasilij korobov <kavyabass51@gmail.com>
2025-04-07 16:32:08 +0700

    Практика:Права доступа

--- users/admin.py
+++ users/admin.py
@@ -5,5 +5,4 @@ from users.models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'phone_number', 'username')
-    search_fields = ('email', 'phone_number', 'username')
+    exclude = ('password',)
